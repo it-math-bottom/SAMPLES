e2421ab0c4d3ef6af1b9c4845595c6f0d5f6bc84
it-math-bottom <free.min1123t@gmail.com>
2022-11-26 11:50:23 +0900

    初期コミット

---
+++ demo-oauth2-min/src/main/java/com/example/demo/oauth2/min/DemoOauth2MinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.oauth2.min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauth2MinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOauth2MinApplication.class, args);
+	}
+
+}
